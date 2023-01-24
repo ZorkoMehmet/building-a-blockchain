3cfc6fcf8e39a5858da6df686353b9d5e75ab954
MEHMET SORKULU <112237375+ZorkoMehmet@users.noreply.github.com>
2023-01-24 22:58:29 +0300

    test

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,19 +1,25 @@
 function Blockchain() {
-    this.chain = [];
-    this.newTransactions = [];
+  this.chain = [];
+  this.newTransactions = [];
 }
 
-Blockchain.prototype.createNewBlock = function (nonce, hash, previousBlockHash){
-    const newBlock = {
-        index: this.chain.length + 1,
-        hash: hash,
-        previousBlockHash: previousBlockHash,
-        timestamp: Date.now(),
-        nonce: nonce,
-        transactions: this.newTransactions
-    }
-    this.newTransactions = [];
-    this.chain.push(newBlock)
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    hash: hash,
+    previousBlockHash: previousBlockHash,
+    timestamp: Date.now(),
+    nonce: nonce,
+    transactions: this.newTransactions,
+  };
+  this.newTransactions = [];
+  this.chain.push(newBlock);
 
-    return newBlock;
-}
+  return newBlock;
+};
+
+module.exports = Blockchain;
