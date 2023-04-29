ee4e03754edf8e59a3d8a85ba0da344081513f62
MEHMET SORKULU <112237375+ZorkoMehmet@users.noreply.github.com>
2023-04-29 14:12:44 -0400

    mining

--- dev/api.js
+++ dev/api.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const Blockchain = require("./blockchain");
-const uuid = require ("uuid/v1");
+const { v1: uuidv1 } = require('uuid');
 
-const nodeAddress = uuid().split("-").join("");
+const nodeAddress = uuidv1().split("-").join("");
 
 const bitcoin = new Blockchain();
 
@@ -21,10 +21,30 @@ app.post("/transaction", function (req, res) {
     req.body.sender,
     req.body.recipient
   );
-  res.json({ note: `Transaction will be in block number ${blockIndex}.` })
+  res.json({ note: `Transaction will be in block number ${blockIndex}.` });
 });
 
-app.get("/mine", function (req, res) {});
+app.get("/mine", function (req, res) {
+  const lastBlock = bitcoin.getLastBlock();
+  const previousBlockHash = lastBlock["hash"];
+  const currentBlockData = {
+    transactions: bitcoin.pendingTransactions,
+    index: lastBlock["index"] + 1,
+  };
+
+  const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+  blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+  const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+  bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+
+  const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+  res.json({
+    note: "New block has been mined successfully.",
+    block: newBlock
+  });
+});
 
 app.listen(3000, function () {
   console.log("Listening on port 3000...");
