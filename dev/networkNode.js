fa472712f6296ac574bc99a864a67c6f7626dca5
MEHMET SORKULU <112237375+ZorkoMehmet@users.noreply.github.com>
2023-05-17 12:47:38 -0400

    consensus

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -77,7 +77,7 @@ app.get("/mine", function (req, res) {
       method: "POST",
       body: { newBlock: newBlock },
       json: true,
-    }; 
+    };
     requestPromises.push(rp(requestOptions));
   });
   Promise.all(requestPromises)
@@ -179,6 +179,49 @@ app.post("/register-nodes-bulk", function (req, res) {
   });
 
   res.json({ note: "Bulk registration successful." });
+});
+
+app.get("/consensus", function (req, res) {
+  const requestPromises = [];
+  bitcoin.networkNodes.forEach((networkNodeUrl) => {
+    const requestOptions = {
+      uri: networkNodeUrl + "/blockchain",
+      method: "GET",
+      json: true,
+    };
+    requestPromises.push(rp(requestOptions));
+  });
+
+  Promise.all(requestPromises).then((blockchains) => {
+    const currentChainLength = bitcoin.chain.length;
+    let maxChainLength = currentChainLength;
+    let newLongestChain = null;
+    let newPendingTransactions = null;
+
+    blockchains.forEach((blockchain) => {
+      if (blockchain.chain.length > maxChainLength) {
+        maxChainLength = blockchain.chain.length;
+        newLongestChain = blockchain.chain;
+        newPendingTransactions = blockchain.pendingTransactions;
+      }
+      if (
+        !newLongestChain ||
+        (newLongestChain && !bitcoin.chainIsValid(newLongestChain))
+      ) {
+        res.json({
+          note: "Current chain has not been replaced.",
+          chain: bitcoin.chain,
+        });
+      } else if (newLongestChain && bitcoin.chainIsValid(newLongestChain)) {
+        bitcoin.chain = newLongestChain;
+        bitcoin.pendingTransactions = newPendingTransactions;
+        res.json({
+          note: "This chain has been replaced.",
+          chain: bitcoin.chain,
+        });
+      }
+    });
+  });
 });
 
 app.listen(port, function () {
